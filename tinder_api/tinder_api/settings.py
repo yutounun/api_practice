9f3ab452a9f504c3c9634af5e30ebc54e5aa7600
Yuto Ichihara <yutounwasese@gmail.com>
2021-12-21 09:22:03 +0900

    add DRF setting in settings.py

--- tinder_api/tinder_api/settings.py
+++ tinder_api/tinder_api/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tinder'
+    # appname
+    'tinder',
+    # framework for API
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
