33d4597495d6bb1927b8db80297fa693ba26f935
Yuto Ichihara <yutounwasese@gmail.com>
2021-12-26 09:44:40 +0900

    ログ出力実装

--- tinder_api/tinder/views.py
+++ tinder_api/tinder/views.py
@@ -12,18 +12,28 @@ from rest_framework.decorators import action
 from .models import Members
 from .serializer import MemberSerializer, JobSerializer
 
+# ログ処理
+import logging
+from logging import getLogger, StreamHandler, Formatter
+
 # 全てのHTTPメソッドに対応
 # 一番シンプルで早く、個人開発向のModelViewSetで実装してみる
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Members.objects.all()
     serializer_class = MemberSerializer
-    
-    # getメソッドでIDが存在しないときはNotFoundエラーを発生させる
+    # 取得したクエリセットを念のためdjango.logとターミナルに出力
+    logger = logging.getLogger('command')
+    logger.info(queryset)
+
     @action(methods=['get'], detail=True)
     def error_handling(self, request, pk=None):
+        # getメソッドでIDが存在しないときはNotFoundエラーを発生させる
         # Memberの指定したidのレコードを取得
         try:
             obj = self.get_object()
+            # objのログをターミナルとファイルに出力
+            logger = logging.getLogger('command')
+            logger.info(obj)
             serializer = self.get_serializer(obj)
             return Response(serializer.data)
         except:
