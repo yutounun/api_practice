f39d72c76e95668df410235379728733caa89d00
Yuto Ichihara <yutounwasese@gmail.com>
2021-12-27 19:38:17 +0900

    各Viewをログイン認証済みのユーザーしか叩けないよう変更

--- tinder_api/tinder/urls.py
+++ tinder_api/tinder/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import *
 from django.db import router
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
 from tinder import views
 
 router = routers.SimpleRouter()
