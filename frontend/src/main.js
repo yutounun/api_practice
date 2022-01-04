c85a402be50753acf52ebb6bcc639c7dea1aafd6
Yuto Ichihara <yutounwasese@gmail.com>
2022-01-04 19:23:09 +0900

    vuetifyを導入

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios' //追記
-import VueAxios from 'vue-axios' //追記
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios"; //追記
+import VueAxios from "vue-axios"; //追記
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueAxios, axios) //追記
+Vue.use(VueAxios, axios); //追記
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import GetMembers from '@/components/GetMembers'
+import GetMembers from "@/components/GetMembers";
 
 Vue.use(VueRouter);
 
