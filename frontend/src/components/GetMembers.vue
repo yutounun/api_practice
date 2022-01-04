c6f44ab88bb7cea086a9ee9a2dd99a514797eba1
Yuto Ichihara <yutounwasese@gmail.com>
2022-01-04 19:29:50 +0900

    ログインフォームとテーブルにvuetifyデザインを適用

--- frontend/src/components/GetMembers.vue
+++ frontend/src/components/GetMembers.vue
@@ -1,27 +1,31 @@
 <template>
   <div>
     <!-- ログイン画面 -->
-    <div class='loginForm' v-if="!tokens.access">
-      <input type="text" placeholder="username" v-model="username">
+    <div class="loginForm" v-if="!tokens.access">
+      <!-- scriptのusernameプロパティに入力値が格納される -->
+      <v-text-field
+        label="username"
+        :rules="rules"
+        hide-details="auto"
+        v-model="username"
+      ></v-text-field>
       <!-- scriptのpasswordプロパティに入力値が格納される -->
-      <input type="text" placeholder="password" v-model="password">
+      <v-text-field label="password" v-model="password"></v-text-field>
       <!-- クリックするとloginメソッドを実行 -->
-      <button @click="login">login</button>
+      <v-btn color="pink lighten-1 white--text" elevation="2" @click="login">LOGIN</v-btn>
       <!-- Membersプロパティから -->
       <!-- scriptのuserプロパティに入力値が格納される -->
     </div>
     <!-- ログイン後画面 -->
     <div v-if="tokens.access">
-      <div v-for="(member, key) in Members" :key="key">
-          <hr>
-          <p>{{ member.username }}</p>
-          <p>{{ member.gender }}</p>
-          <p>{{ member.introduction }}</p>
-          <p>{{ member.job }}</p>
-          <hr>
-        </div>
-      <!-- クリックするとgetInfoメソッドを実行 -->
-      <button @click="getInfo">メンバー情報を取得</button>
+      <v-btn  color="pink lighten-1 white--text" @click="getInfo" elevation="2">メンバー情報を取得</v-btn>
+      <v-data-table
+        :headers="headers"
+        :items="Members"
+        :items-per-page="5"
+        class="elevation-1"
+        v-show="showTable"
+      ></v-data-table>
     </div>
   </div>
 </template>
@@ -30,38 +34,55 @@
 export default {
   data() {
     return {
+      headers: [
+        {
+          text: 'gender',
+          align: 'start',
+          sortable: false,
+          value: 'gender',
+        },
+        { text: 'username', value: 'username' },
+        { text: 'age', value: 'age' },
+        { text: 'introduction', value: 'introduction' },
+        { text: 'job', value: 'job' },
+      ],
       Members: [],
       tokens: {
-        "refresh":'',
-        "access":''
+        refresh: "",
+        access: "",
       },
-      username: '',
-      password: '',
+      username: "",
+      password: "",
+      showTable: false
     };
   },
   methods: {
     // メンバーの各情報を取得
-    getInfo: function(){
+    getInfo: function () {
       this.axios
-      .get("http://127.0.0.1:8000/api/v1/member/",{headers: {
-        // postmanでのAPIcall同様にJWTが必要
-        // 検証モードで確認できるヘッダー
-        "Authorization": 'JWT ' + this.tokens.access,
-      }
-      })
-      // レスポンスをMembersプロパティに格納
-      .then(response => (this.Members = response.data));
+        .get("http://127.0.0.1:8000/api/v1/member/", {
+          headers: {
+            // postmanでのAPIcall同様にJWTが必要
+            // 検証モードで確認できるヘッダー
+            Authorization: "JWT " + this.tokens.access,
+          },
+        })
+        // レスポンスをMembersプロパティに格納
+        .then((response) => {
+          this.showTable = true
+          this.Members = response.data
+        });
     },
     // usernameとpasswordからjwt認証を行いaccess_tokenとrefresh_tokenを取得
-    login: function(){
+    login: function () {
       // token取得のためのusernameとpasswordセット
-      const data = {username: this.username, password: this.password}
+      const data = { username: this.username, password: this.password };
       // access_token&refresh_tokenを取得
       this.axios
-        .post("http://127.0.0.1:8000/api-auth/jwt/",data)
+        .post("http://127.0.0.1:8000/api-auth/jwt/", data)
         // レスポンスを一旦tokensプロパティに格納
-        .then(response => (this.tokens = response.data));
-    }
-  }
+        .then((response) => (this.tokens = response.data));
+    },
+  },
 };
 </script>
