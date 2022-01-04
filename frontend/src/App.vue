6413660e69d185e064af0b811eb5cd7a613cee9e
Yuto Ichihara <yutounwasese@gmail.com>
2022-01-04 20:22:15 +0900

    headerデザイン修正

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="pink lighten-1 white--text" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-icon>mdi-head-heart-outline</v-icon>
+        <h2>TINOER</h2>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/yutounun/api_practice"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <h3 class="mr-2">Github</h3>
+        <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
 
